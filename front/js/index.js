ef2eee254276ffa8c324c2e12561b1885fcc97b2
BoisselNicolas <ncboissel@gmail.com>
2023-03-23 00:19:05 +0100

    fixing

--- front/js/index.js
+++ front/js/index.js
@@ -5,29 +5,5 @@ var animating; //flag to prevent quick multi-click glitches
 
 animating = false;
 
-console.log('into file');
 
-$(".action-button").click(function(){
-
-console.log('into click ')
-	let nextId = $(this).attr("next_id");
-	console.log(nextId)	;
-  
-	
-
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset
-    current_fs.hide();
-    animating = false;
-});
 
