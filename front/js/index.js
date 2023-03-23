060e9782f414680a97344d52d8b51140adab62f1
apersis <119588462+apersis@users.noreply.github.com>
2023-03-23 10:19:02 +0100

    Fix barre de progression

--- front/js/index.js
+++ front/js/index.js
@@ -5,5 +5,10 @@ var animating; //flag to prevent quick multi-click glitches
 
 animating = false;
 
+x = progressbar.getElementsByTagName("LI").length;
+var e = document.getElementsByTagName("LI");
+for (let i=0;i<x;i++){
+	e[i].style.width = (100/x)+'%';
+}
 
 
